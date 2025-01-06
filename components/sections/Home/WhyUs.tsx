5af0e1b503c137cb0191b430236fd441b5bdecd9
Haliyah Tijani <haliyya460@gmail.com>
2025-01-06 23:44:18 +0100

    addition of components

--- components/sections/Home/WhyUs.tsx
+++ components/sections/Home/WhyUs.tsx
@@ -1,7 +1,8 @@
 import { InfoCard } from "@/components/forms/cards/formcard";
-import { whyUsData } from "@/app/helpers/mockData";
+import { whyTdData, whyUsData } from "@/app/helpers/mockData";
+import { TechDiveCard } from "@/components/forms/cards/techCard";
 
-export const TechDiveComponent = () => {
+export const TechDiveComponentRight = () => {
   return (
     <section className="xl:h-[500px] 3xl:max-w-screen-3xl mx-auto bg-[#FFF4E5] p-8 px-5 space-y-[74px]">
       <div className="text-[36px] text-center font-semibold mt-4">
@@ -14,6 +15,26 @@ export const TechDiveComponent = () => {
             icon={item.icon}
             title={item.title}
             description={item.description}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export const TechDiveComponent = () => {
+  return (
+    <section className="3xl:max-w-screen-3xl mx-auto space-y-[74px]">
+      <div className="">
+        {whyTdData.map((item, index) => (
+          <TechDiveCard
+            key={index}
+            title={item.title}
+            description={item.description}
+            imageSrc={item.icon}
+            color={item.color}
+            btnText={item.btnText}
+            isReversed={index % 2 != 1}
           />
         ))}
       </div>
