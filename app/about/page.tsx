84908f223baaa5a2525afac2c9500bbd8bff982a
Ubond Ndoh <ubongndoh@gmail.com>
2025-01-11 15:14:36 +0100

    hero section

---
+++ app/about/page.tsx
@@ -0,0 +1,20 @@
+// import GridSection from "@/components/homepage/GridSection";
+// import { HeroSection } from "@/components/homepage/hero-section";
+// import Services from "@/components/homepage/main-services";
+// import Projects from "@/components/homepage/projects";
+// import { Button } from "@/components/ui/button";
+
+import { HeroSection } from "@/components/about/hero-section";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      {/* <div className="auto-container">
+        <Services />
+        <Projects />
+      </div>
+      <GridSection /> */}
+    </main>
+  );
+}
