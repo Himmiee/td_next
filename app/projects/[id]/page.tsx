f1da8a04fc2c02f74561b000274ac72f5f78740a
Ubond Ndoh <ubongndoh@gmail.com>
2025-01-12 14:14:43 +0100

    hero page added to the project details page

---
+++ app/projects/[id]/page.tsx
@@ -0,0 +1,9 @@
+import { HeroSection } from "@/components/projects/projects-details-page/hero-section";
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
